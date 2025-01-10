bad28205cc0df807e7c3ba6b7efb60d245501af4
Jukeysz <scaglioni.ju@gmail.com>
2025-01-10 18:42:20 -0300

    tweak buffer agenda

--- exercicios/pBufferVs/src/main.c
+++ exercicios/pBufferVs/src/main.c
@@ -3,7 +3,6 @@
 #include <string.h>
 
 // store all data in a single buffer pBuffer
-// find some way of detaching each data and detaching each user
 // juliano fonseca\015\0email@gmail.com\0;mauro\030\0email@gmail.com\0:
 // the very end of the buffer is always ':'
 
@@ -14,76 +13,65 @@ int main( void ) {
 	// 3- search person
 	// 4- list all
 	// 5- exit
+
+	// pBuffer specification (initially has 500 bytes):
+	// op | firstFlag | search | tempName | tempAge | tempEmail | remainingSize | DATASPOT
+	// 4B | 4B        | 30B    | 30B      | 3B      | 40B       | 4B            | rest of the 385 bytes
+
+	// before DATASPOT, we have the intermediate vars, and after it
+	// we have the user data
+
+	// TODO: two functions manipulating the same firstflag variable is messy
 	
-	int *op = (int *)malloc(sizeof(int));
 	void *pBuffer = malloc(500 * sizeof(char));
-	int *firstFlag = (int *)malloc(sizeof(int));
-	*firstFlag = 1;
 	if (pBuffer == NULL) {
 		printf("Failed to allocate memory buffer\n");
 		return 1;
 	}
+	int *op = pBuffer;
+	int *hasUser = pBuffer + 4;
+	*hasUser = 1;
+	
 	
 	// buffer for searching a name
-	char *search = (char *)malloc(40 * sizeof(char));
+	char *search = pBuffer + 8;
 	
 	// temp buffers for adding a user
-	char *tempName = NULL;
-	char *tempAge = NULL;
-	char *tempEmail = NULL;
+	char *tempName = pBuffer + 38;
+	char *tempAge = pBuffer + 68;
+	char *tempEmail = pBuffer + 71;
 
-	int *nameLength = (int *)malloc(sizeof(int));
-	int *ageLength = (int *)malloc(sizeof(int));
-	int *emailLength = (int *)malloc(sizeof(int));
+	// how many bytes to copy after the region to be shrinked 
+	int *remainingSize = pBuffer + 111;
 
-	char *colon = (char *)malloc(sizeof(char));
-	*colon = ':';
-	
 	printf("buffer agenda\n1- add person(Name, Age, Email)\n2- remove person\n3- search person\n4- list all\n5-exit\n");
-	while( scanf( "%i", op ) ) {
+	while( printf("Op:"), scanf( "%i", op ) ) {
 		getchar();
 		switch ( *op ) {
 			// add user
 			case 1: {
-				int *i = (int *)malloc(sizeof(int));
-				*i = 0;
-				char *byteBuffer = pBuffer;
+				// beginning of the data spot
+				char *byteBuffer = pBuffer + 115;
 				// find the offset to copy memory to (only valid if there is already something in the buffer)
-				// this is wrong !!!!!!!!!!!!!!!!!!!
-				if (!(*firstFlag)) {
-					for (*i = 0; byteBuffer[*i] != ':'; (*i)++);
-					// increment by one because I want the position right after the colon
-					(*i)++;
+				if (!(*hasUser)) {
+					while ( *byteBuffer != ':' ) {
+						byteBuffer++;
+					}
+					// stepping after the ':'
+					byteBuffer++;
 				}
 
-				tempName = (char *)malloc(30 * sizeof(char));
-				if (tempName == NULL) {
-					printf("Failed to allocate name buffer\n");
-					return 1;
-				}
 				printf("Enter name: ");
 				fgets(tempName, 30, stdin);
 				// remove the \n
 				tempName[strcspn(tempName, "\n")] = '\0';
 
-
-				tempAge = (char *)malloc(3 * sizeof(char));
-				if (tempAge == NULL) {
-					printf("Failed to allocate age buffer\n");
-					return 1;
-				}
 				printf("Enter age: ");
 				fgets(tempAge, 3, stdin);
 				tempAge[strcspn(tempAge, "\n")] = '\0';
 
-				int *c = (int *)malloc(sizeof(int));
-    			while ((*c = getchar()) != '\n' && c != EOF);
+				getchar();
 
-				tempEmail = (char *)malloc(40 * sizeof(char));
-				if (tempEmail == NULL) {
-					printf("Failed to allocate email buffer\n");
-					return 1;
-				}
 				printf("Enter email: ");
 				fgets(tempEmail, 40, stdin);
 				tempEmail[strcspn(tempEmail, "\n")] = '\0';
@@ -91,30 +79,24 @@ int main( void ) {
 				// I have three buffers, I have to copy them to pBuffer with the appropriate offset,
 				// each information is delimited by \0
 				// it is pertinent to use memcpy perhaps
-				
-				// It is the length + 1 because I also want to copy the \0
-				*nameLength = strlen(tempName) + 1;
-				*ageLength = strlen(tempAge) + 1;
-				*emailLength = strlen(tempEmail) + 1;
-				
-				// if we are adding a user and there are already some, adjust the old colon into a semicolon
-				if (!*firstFlag) {
-					char *adjustSemicolon = pBuffer + *i - 1;
-					*adjustSemicolon = ';';
+
+				// if I am adding a user and there are already some, adjust the old colon into a semicolon
+				if (!*hasUser) {
+					*(byteBuffer - 1) = ';';
 				}
 				
 				// base offset + pBuffer
-				memcpy(*i + pBuffer, tempName, *nameLength);
+				memcpy(byteBuffer, tempName, strlen(tempName) + 1);
 				// base offset + pBuffer + the previous data offset (+1 because I need the index after the \0)
-				memcpy(*i + pBuffer + *nameLength, tempAge, *ageLength);
+				memcpy(byteBuffer + strlen(tempName) + 1, tempAge, strlen(tempAge) + 1);
 				// base offset + pBuffer + the two previous data offset
-				memcpy(*i + pBuffer + *nameLength + *ageLength, tempEmail, *emailLength);
+				memcpy(byteBuffer + strlen(tempName) + 1 + strlen(tempAge) + 1, tempEmail, strlen(tempEmail) + 1);
 				
 				// add the final colon for marking the buffer's end
 				// this is of subtle importance when calculating the offset for the next person
-				memcpy(*i + pBuffer + *nameLength + *ageLength + *emailLength, colon, strlen(colon));
-
-				*firstFlag = 0;
+				//memcpy(byteBuffer + *nameLength + *ageLength + *emailLength, colon, strlen(colon));
+				*(byteBuffer + strlen(tempName) + 1 + strlen(tempAge) + 1 + strlen(tempEmail) + 1)  = ':';
+				*hasUser = 0;
 
 				break;
 			} break;
@@ -125,33 +107,21 @@ int main( void ) {
 				// if I want to remove joao
 				// the first parameter is the destination, that is to say, shrink the data after the joao's region
 				// into the beginning of joao's region
-				char *username = (char *)malloc(sizeof(char) * 30);
-				if (username == NULL) {
-					printf("Failed memory allocation\n");
-					return 1;
-				}
+
+				// i've got to handle the particular case which there is a single user
+				char *username = search;
 
 				printf("Enter the name to be removed: ");
 				fgets(username, 30, stdin);
 				username[strcspn(username, "\n")] = '\0';
 
-				char *byteBuffer = NULL;
-				char *j = malloc(sizeof(char));
-				if (j == NULL) {
-					printf("Failed memory allocation\n");
-					return 1;
-				}			
+				char *byteBuffer;
+				// change this variable, its purpose is telling whether or not the user is at the beginning
+				*hasUser = 1;
 
-				int *beginning = malloc(sizeof(int));
-				if (beginning == NULL) {
-					printf("Failed memory allocation\n");
-					return 1;
-				}						
-				*beginning = 1;
-
-				for (byteBuffer = pBuffer; *byteBuffer != ':'; byteBuffer++) {
-					if (*byteBuffer == ';' || beginning) {
-						if (!beginning) {
+				for (byteBuffer = pBuffer + 115; *byteBuffer != ':'; byteBuffer++) {
+					if (*byteBuffer == ';' || *hasUser) {
+						if (!(*hasUser)) {
 							byteBuffer++;
 						}
 						// takes the beginning of byte buffer up to the next \0
@@ -159,19 +129,19 @@ int main( void ) {
 							// once I found the string, take the beginning into account 
 							// and find the index of the next ';' or ':'
 							// remaining points to the beginning of the section that comes after the chunk that has to be removed
+
+							// the beginning of the region to be overwritten is the ';'
+							// so bytebuffer - 1
 							char *remaining = byteBuffer;
+							byteBuffer -= 1;
 							char *auxRemaining = NULL;
 							
 							while (*remaining != ':' && *remaining != ';') {
 								remaining++;
 							}
 
-							/// remaining ends up pointing at one address before the ':' or ';'
-							// this is why I use remaining++
-							remaining++;
-
+							/// remaining ends up pointing at one address that has ':' or ';'
 							auxRemaining = remaining;
-							size_t *remainingSize = malloc(sizeof(size_t));
 							// 1 because there has to be at least one ';' or ':'
 							*remainingSize = 1;						
 							while (*auxRemaining != ':') {
@@ -181,26 +151,25 @@ int main( void ) {
 
 							memmove(byteBuffer, remaining, *remainingSize);
 
-							// clean up the old bytes that were shrinked
-							memset(remaining, '0', remainingSize);
-
 							break;
 						}
 					}
+					*hasUser = 0;
 				}
 				printf("Username not found\n");
 				break;
 			}
 			case 4: {
-				if ( pBuffer != NULL ) {
+				char *byteBuffer = pBuffer + 115;
+				if ( byteBuffer != NULL ) {
 					// TODO
 					// Print only the people names
 					// first one is easy, no need to adjust offset
-					if (pBuffer == NULL) {
+					if (byteBuffer == NULL) {
 						printf("No users\n");
 						break;
 					} else {
-						char *word = pBuffer;
+						char *word = byteBuffer;
 						printf("%s\n", word);
 						
 						// stop at the next semicolon, breaking if it finds any ':'
@@ -218,34 +187,8 @@ int main( void ) {
 				}
 			}
 			case 5: {
-				if ( pBuffer != NULL ) {
-					free(pBuffer);
-				}
-				if ( ageLength != NULL ) {
-					free(ageLength);
-				}
-				if ( nameLength != NULL ) {
-					free(nameLength);
-				}
-				if ( emailLength != NULL ) {
-					free(emailLength);
-				}
-				if (colon != NULL) {
-					free(colon);
-				}
-				if ( tempName != NULL ) {
-					free(tempName);
-				} 
-				if ( tempAge != NULL ) {
-					free( tempAge );
-				}
-				if (tempEmail != NULL) {
-					free( tempEmail );
-				}
-				if (firstFlag != NULL) {
-					free( firstFlag );
-				}
-
+				free(pBuffer);
+				printf("Program finished!\n");
 				return 0;
 			}
 		}
